ecbd9a81a75d7915b28aec71dc41e18f7779664c
b1n01 <chielorenz@gmail.com>
2022-05-26 23:47:38 +0200

    Update elements tests

--- test/elem.test.js
+++ test/elem.test.js
@@ -5,47 +5,52 @@ const exception = "EXCEPTION";
 const tests = [
   {
     t: "Paragraph",
-    i: ["Hello"],
+    i: ["Hello", " Hello "],
     o: [{ type: "p", value: [{ type: "string", value: "Hello" }] }],
   },
   {
     t: "h1",
-    i: ["# Hello"],
+    i: ["# Hello", "#Hello", " # Hello "],
     o: [{ type: "h1", value: [{ type: "string", value: "Hello" }] }],
   },
   {
     t: "h2",
-    i: ["## Hello"],
+    i: ["## Hello", "##Hello", " ## Hello "],
     o: [{ type: "h2", value: [{ type: "string", value: "Hello" }] }],
   },
   {
     t: "h3",
-    i: ["### Hello"],
+    i: ["### Hello", "###Hello", " ### Hello "],
     o: [{ type: "h3", value: [{ type: "string", value: "Hello" }] }],
   },
   {
     t: "h4",
-    i: ["#### Hello"],
+    i: ["#### Hello", "####Hello", " #### Hello "],
     o: [{ type: "h4", value: [{ type: "string", value: "Hello" }] }],
   },
   {
     t: "h5",
-    i: ["##### Hello"],
+    i: ["##### Hello", "#####Hello", " ##### Hello "],
     o: [{ type: "h5", value: [{ type: "string", value: "Hello" }] }],
   },
   {
     t: "h6",
-    i: ["###### Hello"],
+    i: ["###### Hello", "######Hello", " ###### Hello "],
     o: [{ type: "h6", value: [{ type: "string", value: "Hello" }] }],
   },
   {
+    t: "Seven hashes",
+    i: ["####### Hello"],
+    o: [{ type: "h6", value: [{ type: "string", value: "# Hello" }] }],
+  },
+  {
     t: "Blockquote",
-    i: ["> Hello"],
+    i: ["> Hello", ">Hello", " > Hello "],
     o: [{ type: "blockquote", value: [{ type: "string", value: "Hello" }] }],
   },
   {
     t: "Pre",
-    i: ["| Hello"],
+    i: ["| Hello", "|Hello", " | Hello "],
     o: [
       {
         type: "pre",
@@ -55,7 +60,7 @@ const tests = [
   },
   {
     t: "Strong",
-    i: ["**Hello**"],
+    i: ["**Hello**", " **Hello** "],
     o: [
       {
         type: "p",
@@ -67,7 +72,7 @@ const tests = [
   },
   {
     t: "Italic",
-    i: ["__Hello__"],
+    i: ["__Hello__", " __Hello__ "],
     o: [
       {
         type: "p",
@@ -79,7 +84,7 @@ const tests = [
   },
   {
     t: "Strike",
-    i: ["~~Hello~~"],
+    i: ["~~Hello~~", " ~~Hello~~ "],
     o: [
       {
         type: "p",
@@ -91,7 +96,7 @@ const tests = [
   },
   {
     t: "Code",
-    i: ["``Hello``"],
+    i: ["``Hello``", " ``Hello`` "],
     o: [
       {
         type: "p",
@@ -101,7 +106,7 @@ const tests = [
   },
   {
     t: "h1 with strong",
-    i: ["# **Hello**"],
+    i: ["# **Hello**", "#**Hello**", " # **Hello** "],
     o: [
       {
         type: "h1",
@@ -114,13 +119,11 @@ const tests = [
 ];
 
 tests.forEach(({ t: label, i: inputs, o: output }) => {
-  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-
   inputs.forEach((input) => {
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
     parser.feed(input);
     const results = parser.results;
     test(label, () => expect(results[0]).toEqual(output));
-    // Test that the given input didn't produce more than one output
     test(`Ambiguity for ${label}`, () => expect(results.length).toBe(1));
   });
 });
